d34b679968d50621a72c9bebf1f1de7e54dbd1e4
vaishnavi773 <155604199+vaishnavi773@users.noreply.github.com>
2025-01-07 15:35:44 +0530

    Delete Simple_Calculator.php

--- Simple_Calculator.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$num1=10;
-	$num2=20;
-	echo "Addition :  ".($num1+$num2)."<br>";
-	echo "Substraction :  ".($num1-$num2)."<br>";
-	echo "Multiplication :  ".($num1*$num2)."<br>";
-	echo "Division :  ".($num1/$num2)."<br>";
-
-?>
